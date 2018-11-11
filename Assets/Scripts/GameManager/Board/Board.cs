e2796bef5bd8babc08ac60b7421612c26bbf7331
Leo <leojh.li@mail.utoronto.ca>
2018-11-10 21:29:31 -0500

    created Board class and added grid to unity position conversion method to it

---
+++ Assets/Scripts/GameManager/Board/Board.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public float tileWidth;
+    public float tileHeight;
+
+    public static Vector2 CoordsToPosition(int x, int y)
+    {
+        return new Vector2((y-x)*tileWidth, (x+y)*-tileHeight);
+    }
+}

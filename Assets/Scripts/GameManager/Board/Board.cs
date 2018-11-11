91c7fba6a8f6e150314cf1b7c2de2b96dd71e395
Leo <leojh.li@mail.utoronto.ca>
2018-11-10 21:36:41 -0500

    added tile testing methods

--- Assets/Scripts/GameManager/Board/Board.cs
+++ Assets/Scripts/GameManager/Board/Board.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
+    public GameObject tile;         // Base tile for testing
     public float tileWidth;
     public float tileHeight;
+
+    void Start()
+    {
+        DrawBoard();
+    }
+
+    // Test drawing the board
+    public static void DrawBoard(int rows, int cols)
+    {
+        foreach (int i in rows)
+        {
+            foreach (int j in cols)
+            {
+                Instantiate(tile, CoordsToPosition(i, j));
+            }
+        }
+    }
 
     public static Vector2 CoordsToPosition(int x, int y)
     {

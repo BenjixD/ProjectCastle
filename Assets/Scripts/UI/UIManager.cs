aa154e9b964ac3097aa7cbdc94d04c5948c91826
Leo <lileo-jh@gmail.com>
2018-11-13 02:21:26 -0500

    added prototype timeline display

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public int maxFrames;
+    public Image timelineDisplay;
+    public Image pointer;
+
+    private float timelineWidth;
+    private float frameWidth;
+    public float frameTickOffset;
+    private float pointerY;
+    private RectTransform pointerTransform;
+
+    void Start()
+    {
+        timelineWidth = timelineDisplay.GetComponent<RectTransform>().rect.width;
+        frameWidth = timelineWidth / maxFrames + frameTickOffset;
+        pointerTransform = pointer.GetComponent<RectTransform>();
+        pointerY = pointerTransform.anchoredPosition.y;
+    }
+
+    public void UpdateTimelineDisplay(int frame)
+    {
+        pointerTransform.anchoredPosition = new Vector2(-timelineWidth / 2 + (frame * frameWidth), pointerY);
+    }
+}

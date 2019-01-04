c2c62602205a9de63f6c7345bf5fdf64032053f6
Leo <lileo-jh@gmail.com>
2019-01-04 18:40:47 -0500

    Added 3 tile slash attack

---
+++ Assets/Scripts/Unit/Action/SwordSlash/SwordSlash.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSlash : Action {
+	void Start() {
+		//Add frames in order
+		this.frames = new List<Frame>();
+        this.frames.Add(new SwordSlashFrameAttack());
+        this.frames.Add(new SwordSlashFrameAttack());
+        this.frames.Add(new SwordSlashFrameEnd());
+        this.frames.Add(new SwordSlashFrameEnd());
+	}
+
+    public override IEnumerator Select(Unit unit, Board board, Timeline timeline, IEnumerator next)
+    {
+        ActionUI ui = Instantiate(actionUI.gameObject, gameObject.transform).GetComponent<ActionUI>();
+        ui.Initialize(unit, this, board, timeline);
+        while (ui.state != ActionSubmissionState.SUBMITTED && ui.state != ActionSubmissionState.CANCELLED)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        Destroy(ui.gameObject);
+        StartCoroutine(next);
+        yield return null;
+    }
+}

b9359c44e262becb5d9c41fe10ce51905e8b9313
Leo <lileo.jh+github@gmail.com>
2019-01-15 23:09:16 -0500

    Removed extra line

--- Assets/Scripts/Unit/Action/PowerShot/Anim/PowerShotFrameAnimAttack.cs
+++ Assets/Scripts/Unit/Action/PowerShot/Anim/PowerShotFrameAnimAttack.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PowerShotFrameAnimAttack : FrameAnim {
 
-	public const int DMG = 60;
-
 	public PowerShotFrameAnimAttack(Action instance) : base(instance) {}
 
 	public override bool ExecuteAnimation(SimulatedDisplacement sim, Direction dir, Board board) {
